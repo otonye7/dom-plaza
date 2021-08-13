a1b438ef505d046312225b13ae45a6b28acad536
otonye <otonyeamie@gmail.com>
2021-08-14 00:44:46 +0100

    master

--- src/components/display/display.component.jsx
+++ src/components/display/display.component.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { DisplayContainer } from './display.styles';
 
 
 const Display = () => {
+    const [getAgenda, setAgenda] = useState([])
+    
+    useEffect(() => {
+        loadAgenda()
+    }, [])
+
+    const loadAgenda = async () => {
+        let res = await axios.get(`http://localhost:8000/api/get-agenda`)
+        console.log(res)
+    }
+
     return (
     <DisplayContainer>
         <div className='container'>
